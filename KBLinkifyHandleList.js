b6f8a06dfe34144dd0b247b730ffaf401240926b
Brian Stamper <stamper.10@osu.edu>
2014-06-26 13:50:13 -0400

    Initial commit, uploaded scripts from local.

---
+++ KBLinkifyHandleList.js
@@ -0,0 +1,32 @@
+var module_name = "KB linkify list of handles";
+var module_ver = "0.1";
+/*
+    (This is a script for the PSPad editor, see http://www.pspad.com/. I have no
+	 affiliation with the developers of PSPad. These scripts are offered as-is,
+	 use at your own risk.)
+    
+
+    Takes a list of handle.net identifiers and turns them into KB links.
+    Identifiers are of the form ####/#####, e.g., 1811/48299
+    
+    Brian Stamper
+    
+*/
+function KBLinkifyHandleList()
+{	                                    
+  var thisdoc = newEditor();          
+  thisdoc.assignActiveEditor();       
+  thistext = thisdoc.Text();          
+  thistext = thistext.replace(/[^\r\w]/g, " "); // non-word into single space
+  thistext = thistext.replace(/^\s*/gm, ""); // trim whitespace front
+  thistext = thistext.replace(/\s*$/gm, ""); // trim whitespace back
+  thistext = thistext.replace(/[^\r\S]+/g, "%20"); // any space into single URL-encoded space
+  thistext = thistext.replace(/^(.*?)$/gm, "<a href=\"https://kb.osu.edu/dspace/handle/$1?show=full\">$1</a><br />"); //build KB URL
+  
+  thisdoc.Text(thistext);             
+                                      
+}                                     
+                                      
+function Init(){                        
+  addMenuItem("KB linkify list of handles", "", "KBLinkifyHandleList");
+}                                     
